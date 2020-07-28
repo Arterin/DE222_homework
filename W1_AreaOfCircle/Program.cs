0537feb5e7408f3684e1315365ce35e14a3c77b8
Vivian Luxon <arterinv@gmail.com>
2020-07-29 06:56:23 +1200

    initial commit

---
+++ W1_AreaOfCircle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Week1Homework
+{
+    class Program
+    {
+        static void Main()
+        {
+            var circle = new Circle(2.5);
+            var circleArea = circle.GetArea();
+            Console.WriteLine($"The area of the circle is {circleArea}");
+        }
+    }
+
+    public class Circle
+    {
+        // Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Properties
+        public double Radius{ get; set; }
+        public const double Pi = 3.14;
+        public double Diameter { get; set; }
+        public double Area { get; set; }
+        
+        // Methods
+        public double GetArea()
+        {
+            Diameter = Radius * 2;
+            Area = Math.Round(Diameter * Pi, 2);
+            return Area;
+        }
+    }
+}
