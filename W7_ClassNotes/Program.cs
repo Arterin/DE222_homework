091d5983eaa8f525d2fc87860a695792cbdaa926
Vivian Luxon <arterinv@gmail.com>
2020-09-07 10:19:10 +1200

    Initial commit

---
+++ W7_ClassNotes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace W6_ClassNotes
+{
+    internal class LooksLikeCollection<T>
+    {
+        private readonly T[] _data = new T[100];
+
+        public T this[int i]
+        {
+            get => _data[i];
+            set => _data[i] = value;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var greetings = new LooksLikeCollection<string>();
+            greetings[0] = "hello";
+            greetings[1] = "world";
+
+            Console.WriteLine(greetings[1]);
+        }
+    }
+}
